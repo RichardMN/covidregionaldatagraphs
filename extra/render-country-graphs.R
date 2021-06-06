ae500652cc66522631c1aa57908ddd9f7647e3da
Richard Martin-Nielsen <richard.martin@utoronto.ca>
2021-06-06 17:57:56 +0300

    Add safely for rendering, uncomment possibly-problematic countries.

--- extra/render-country-graphs.R
+++ extra/render-country-graphs.R
@@ -5,25 +5,26 @@ library(purrr)
 countries <- get_available_datasets(type = "regional")
 
 render_graphs = function(country) {
-  rmarkdown::render(
-    "extra/Country-graphs.Rmd",
-    output_format = "github_document",
-    output_options = list(
+  safely(
+    rmarkdown::render(
+      "extra/Country-graphs.Rmd",
       output_format = "github_document",
-      self_contained = FALSE
-    ),
-    params = list(
-      country = country,
-      prepared_by = "github.com/RichardMN/covidregionaldatagraphs"
-    ),
-    output_file = paste0("Report ", country, ".md")
+      output_options = list(
+        output_format = "github_document",
+        self_contained = FALSE
+      ),
+      params = list(
+        country = country,
+        prepared_by = "github.com/RichardMN/covidregionaldatagraphs"
+      ),
+      output_file = paste0("Report ", country, ".md")
+    )
   )
 }
 
-countries <- countries %>%
-  filter(!class %in% c("Brazil", "Mexico", "USA", "UK") )
+#countries <- countries %>%
+#  filter(!class %in% c("Brazil", "Mexico", "USA", "UK") )
 
 start_using_memoise()
 
 purrr::map(countries$origin, render_graphs )
-
