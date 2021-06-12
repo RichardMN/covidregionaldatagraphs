e44d0d62211cdb100409c8bb7804d55f2925d7e9
Richard Martin-Nielsen <richard.martin@alumni.utoronto.ca>
2021-06-12 22:23:15 +0300

    Add R code to render US and UK graphs

---
+++ extra/render-US-UK.R
@@ -0,0 +1,29 @@
+
+library(covidregionaldata)
+library(dplyr)
+library(purrr)
+countries <- get_available_datasets(type = "regional")
+
+render_graphs = function(country) {
+    rmarkdown::render(
+      "extra/Country-graphs.Rmd",
+      output_format = "github_document",
+      output_dir = "extra/output",
+      output_options = list(
+        output_format = "github_document",
+        self_contained = FALSE
+      ),
+      params = list(
+        country = country,
+        prepared_by = "github.com/RichardMN/covidregionaldatagraphs"
+      ),
+      output_file = paste0("COVID-19 regional graphs - ", country, ".md")
+    )
+}
+
+countries <- countries %>%
+  filter(class %in% c("USA", "UK") )
+
+start_using_memoise()
+
+purrr::map(countries$origin, render_graphs )
